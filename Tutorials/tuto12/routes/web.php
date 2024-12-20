6764c390b3a5f5a16cd63c5f2d8f55996788a13a
Aminebennani1 <mohammedaminebennanikabchi@gmail.com>
2024-12-20 12:36:11 +0000

    tuto12

---
+++ Tutorials/tuto12/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\Article;
+use App\Models\Tag;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/Article', function () {
+//     $article = Article::find(1);
+// $tag1 = Tag::create(['name' => 'PHP']);
+// $tag2 = Tag::create(['name' => 'Laravel']);
+// $article->tags()->attach([$tag1->id, $tag2->id]);
+//   dd($article);
+// });
